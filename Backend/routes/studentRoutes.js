dd909754df09180ff953f46c8b4d4bb2af44f194
Ramzan <muhammadramzan3043@gmail.com>
2025-12-20 12:26:36 +0500

    ramzan

--- Backend/routes/studentRoutes.js
+++ Backend/routes/studentRoutes.js
@@ -6,10 +6,10 @@ import {
     updateStudent,
     deleteStudent,
 } from "../controllers/studentController.js";
-
+import { upload } from "../middleware/uploads.js";
 const router = express.Router();
 
-router.post("/create", createStudent);
+router.post("/create", upload.single('photo'), createStudent);
 router.get("/show", getStudents);
 router.get("/single/:id", getSingleStudent);
 router.put("/update/:id", updateStudent);
